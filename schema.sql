dfa68b43b43986728be4318e20665d09d1947efa
ryan <ryan91396@gmail.com>
2016-11-20 16:13:42 -0500

    updated sql code

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-create table customer (cid varchar(20), pass varchar(20));
-create table staff ( sid varchar(20), pass varchar(20), isMgr char(1));
-create table merch (mid varchar(20), quantity int, price double, discount double);
-create table orders (oid varchar(20), cid varchar(20), mid varchar(20), status varchar(8), dateMade datetime, totalPrice double);
+create table customer (cid varchar(20) UNIQUE, pass varchar(20));
+create table staff ( sid varchar(20) UNIQUE, pass varchar(20), isMgr char(1));
+create table merch (mid varchar(20) UNIQUE, quantity int, price double, discount double);
+create table orders (oid varchar(20) UNIQUE, cid varchar(20), mid varchar(20), status varchar(8), dateMade datetime, totalPrice double);
