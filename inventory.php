8eef86cfb058a352446236e2e42313292ff0db4e
Alijah Green <atra2396@gmail.com>
2016-11-21 00:58:46 -0500

    I think they work

---
+++ inventory.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+
+		<?php
+			$servername = "rash222.netlab.uky.edu";
+			$username = "root";
+			$password = $username;
+
+			$conn = new mysqli($servername, $username, $password, 'PROJECT');
+
+		?>
+
+		<title>Inventory</title>
+
+		<style type="text/css">
+
+			div.user_info {
+				position: absolute;
+				top: 0px;
+				right: 10px;
+				width: 100px;
+
+				text-align: right;
+				font-size: 11pt;
+			}
+
+<!-			div.inventory {
+				position: absolute;
+				top: 10%;
+				left: 5%;
+				
+				font-size: 65pt;
+				font-family: Verdana, sans-serif;
+			}
+
+			div.inventory_table {
+				position: absolute;
+				top: 15%;
+				left: 5%;
+			}
+-!>
+		</style>
+
+	</head>
+
+	<body>
+		<?php
+	
+			if(isset($_POST["item"])){
+				$query = "UPDATE merch SET quantity = ".$_POST["qty"]." price = ".$_POST["price"]. "WHERE mid = '".$_POST['item'];
+				$resp = $conn->query($query);
+
+				echo "<p>Updated item entry.</p>"
+			}
+
+			else {
+
+		?>
+
+
+		<div class="inventory">Inventory</div>
+		<input type="text" name="item" value="Item">
+		<input type="text" name="qty" value = "Value">
+		<input type="text" name="price" value = "Price">
+		<input type="submit" method="POST" value="Submit">
+
+		<?php } ?>
+
+		<div class="user_info">
+			Hello, user<br>
+			<a href="./shipments.php">Shipments</a><br>
+			<a href="./index.html">Logout</a>
+		</div>
+
+	</body>
+
+</html>
---
+++ shipments.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<?php
+			$servername = "rash222.netlab.uky.edu";
+			$username = "root";
+			$password = $username;
+
+			$conn = new mysqli($servername, $username, $password, 'PROJECT');
+		?>		
+		
+		<title>Shipments</title>
+
+		<style type="text/css">
+
+			div.user_info {
+				position: absolute;
+				top: 0px;
+				right: 10px;
+				width: 100px;
+
+				text-align: right;
+				font-size: 11pt;
+			}
+
+			div.pending_shipments {
+				position: absolute;
+				top: 10%;
+				left: 5%;
+				
+				font-size: 65pt;
+				font-family: Verdana, sans-serif;
+			}
+
+			div.shipments_table {
+				position: absolute;
+				top: 15%;
+				left: 5%;
+			}
+
+		</style>
+
+	</head>
+
+	<body>
+
+		<?php 
+			if(isset($_POST['Ship'])){ 
+				$query = "SELECT mid, quantity, totalPrice FROM orders WHERE status = 'pending'";
+				$result = $conn->query($query);
+
+				$stock_query = "SELECT mid, quantity FROM merch WHERE merch.mid IN orders";
+				$stock_result = $conn->query($stock_query);
+
+				while($row = $result->fetch_assoc()){
+					$item = $stock_result->fetch_assoc();
+					if($item["quantity"] > $row["quantity"]){
+						$update_query = "UPDATE merch SET quantity = ". $item["quantity"] - $row["quantity"] ."WHERE merch.mid=\"". $item["mid"]."\"; UPDATE orders SET status = 'shipped' WHERE status = 'pending'";
+						$update_result = $conn->query($update_query);
+					}
+				}
+			}
+
+			echo "<p>Orders shipped.</p>"
+
+			else {
+		?>
+
+		<div class="pending_shipments">Pending Shipments</div>
+
+		<div class="shipments_table">
+			<?php
+				$query = "SELECT mid, quantity, totalPrice FROM orders";
+				$result = $conn->query($query);
+				$attributes = array('mid','quantity','totalPrice');
+
+				echo "<form action=\"./shipments.php\" method=\"POST\">";
+				echo "<input type=\"submit\" value=\"Ship\">";
+				echo "</form>"
+				echo "<table style=\"border: 1px solid black; border-spacing: 5px\"";
+				echo "<tr>";
+
+				foreach($attribute as $heading) {
+					echo "<th>" . $heading . "</th>";
+				}
+				echo "</tr>";
+
+				while($row = $result->fetch_assoc()){
+
+					echo "<tr>";
+					echo "<td>" . $row['mid'] . "</td>";
+					echo "<td>" . $row['quantity'] . "</td>";
+					echo "<td>" . $row['price'] . "</td>";
+					echo "</tr>";
+				}
+
+				echo "</table>";
+				mysqli_free_result($result);
+	
+			?>
+		</div>
+
+		<?php } ?>
+
+		<div class="user_info">
+			Hello, user<br>
+			<a href="./inventory.html">Inventory</a><br>
+			<a href="./index.html">Logout</a>
+		</div>
+
+	</body>
+
+</html>
