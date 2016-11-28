77d7c2c8c7465c9159d9aac48d71969f1b3ca142
Alijah Green <atra2396@gmail.com>
2016-11-28 09:39:40 -0500

    STUFF

--- shipments.php
+++ shipments.php
@@ -47,7 +47,7 @@
 	<body>
 
 		<?php 
-			if(!empty($_POST)){ 
+			if(!empty($_GET)){ 
 				$query = "SELECT mid, quantity, totalPrice FROM orders WHERE status = 'pending'";
 				$result = $conn->query($query);
 
@@ -74,7 +74,7 @@
 				$result = $conn->query($query);
 				$attributes = array('mid','quantity','totalPrice');
 
-				echo "<form method=\"POST\">";
+				echo "<form action=\"./shipments.php\" method=\"GET\">";
 				echo "<input type=\"submit\" value=\"Ship\" name=\"Ship_Order\">";
 				echo "</form>";
 				echo "<table style=\"border: 1px solid black; border-spacing: 5px\"";
