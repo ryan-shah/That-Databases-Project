5b639fcd86e355823bf8a5c8952eb5fba56b8104
Alijah Green <atra2396@gmail.com>
2016-12-06 14:14:40 -0500

    Fixed overwritting of session details

--- stats.php
+++ stats.php
@@ -10,7 +10,6 @@
 	<head>
 		<?php
 			session_start();
-			$_SESSION['position'] = 'staff';
 			$servername = "rash227.netlab.uky.edu";
 			$username = "root";
 			$password = $username;
@@ -23,35 +22,39 @@
 		<?php navbarStaff(); ?>
 		<p>Sales from last...<br></p>
 		<?php
-			echo "<table>";
-			echo "<th>Time</th>";
-			echo "<th>Amount</th>";
-			$query = 'SELECT SUM(totalPrice) FROM orders WHERE dateMade > (curdate() - INTERVAL 1 WEEK) AND status = "shipped"';
-			$result = $conn->query($query);
-			$result = $result->fetch_row();
-			echo "<tr>";
-			echo "<td>Week</td>";
-			echo "<td>".$result[0]."</td>";
-			$query = 'SELECT SUM(totalPrice) FROM orders WHERE dateMade > (curdate() - INTERVAL 1 MONTH) AND status = "shipped"';
-			$result = $conn->query($query);
-			$result = $result->fetch_row();
-			echo "<tr>";
-			echo "<td>Month</td>";
-			echo "<td>".$result[0]."</td>";
-			$query = 'SELECT SUM(totalPrice) FROM orders WHERE dateMade > (curdate() - INTERVAL 1 YEAR) AND status = "shipped"';
-			$result = $conn->query($query);
-			$result = $result->fetch_row();
-			echo "<tr>";
-			echo "<td>Year</td>";
-			echo "<td>".$result[0]."</td>";
-			$query = 'SELECT SUM(totalPrice) FROM orders WHERE status = "shipped"';
-			$result = $conn->query($query);
-			$result = $result->fetch_row();
-			echo "<tr>";
-			echo "<td>All Time</td>";
-			echo "<td>".$result[0]."</td>";
-
-			echo '</table>';
+			if($_SESSION['position'] === 'manager'){
+				echo "<table>";
+				echo "<th>Time</th>";
+				echo "<th>Amount</th>";
+				$query = 'SELECT SUM(totalPrice) FROM orders WHERE dateMade > (curdate() - INTERVAL 1 WEEK) AND status = "shipped"';
+				$result = $conn->query($query);
+				$result = $result->fetch_row();
+				echo "<tr>";
+				echo "<td>Week</td>";
+				echo "<td>".$result[0]."</td>";
+				$query = 'SELECT SUM(totalPrice) FROM orders WHERE dateMade > (curdate() - INTERVAL 1 MONTH) AND status = "shipped"';
+				$result = $conn->query($query);
+				$result = $result->fetch_row();
+				echo "<tr>";
+				echo "<td>Month</td>";
+				echo "<td>".$result[0]."</td>";
+				$query = 'SELECT SUM(totalPrice) FROM orders WHERE dateMade > (curdate() - INTERVAL 1 YEAR) AND status = "shipped"';
+				$result = $conn->query($query);
+				$result = $result->fetch_row();
+				echo "<tr>";
+				echo "<td>Year</td>";
+				echo "<td>".$result[0]."</td>";
+				$query = 'SELECT SUM(totalPrice) FROM orders WHERE status = "shipped"';
+				$result = $conn->query($query);
+				$result = $result->fetch_row();
+				echo "<tr>";
+				echo "<td>All Time</td>";
+				echo "<td>".$result[0]."</td>";
+				echo '</table>';
+			}
+			else {
+				echo "Go away";
+			}
 		?>
 	</body>
 
